d5bd2a505fe9864b7b6214ae7af15d429ce1d1e7
Zainab Nawaz <124915885+zaza734@users.noreply.github.com>
2025-03-25 13:13:26 -0600

    Create Schedule.java

---
+++ src/Schedule.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+import java.sql.Time;
+
+public class Schedule {
+    int scheduleID;
+    Driver driver;
+    Vehicle vehicle;
+    RideRequest rideRequest;
+    Date scheduledDate;
+    Time scheduledTime;
+
+    
+
+}
