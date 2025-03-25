dc9d7680be842ecc8b1197b1e2f7de5f59bd8edf
LandonWhitford <landon.whitford11@gmail.com>
2025-03-25 12:29:29 -0600

    Create RideRequest.java

---
+++ src/edu/ucalgary/oop/RideRequest.java
@@ -0,0 +1,17 @@
+package edu.ucalgary.oop;
+
+public class RideRequest {
+
+    // Private data Members
+    private int requestID;
+    private String clientName;
+    private String pickUpLocation;
+    private String dropOffLocation;
+    private int passengerCount;
+    private String specialRequirements;
+    private Date requestDate;
+    private Time pickupTime;
+    private String status;
+
+    // Getter methods
+}
