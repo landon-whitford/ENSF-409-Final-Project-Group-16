f9f376322b0884ee1d71d9e765eb3fe159a3c300
Zainab Nawaz <124915885+zaza734@users.noreply.github.com>
2025-04-01 13:56:33 -0600

    Update RideRequest.java

--- src/edu/ucalgary/oop/RideRequest.java
+++ src/edu/ucalgary/oop/RideRequest.java
@@ -14,4 +14,40 @@ public class RideRequest {
     private String status;
 
     // Getter methods
+    public int getRequestID() { return this.requestID; }
+
+    public String getClientName() {return this.clientName;}
+
+    public String getPickUpLocation() {return this.pickUpLocation;}
+
+    public String getDropOffLocation() {return this.dropOffLocation;}
+
+    public int getPassengerCount() { return this.passengerCount;}
+
+    public String getSpecialRequirements() {return this.specialRequirements;}
+
+    public Date getRequestDate() { return this.requestDate;}
+
+    public Time getPickupTime() {return this.pickupTime;}
+
+    public String getStatus() {return this.status};
+
+    // Setter Methods
+    public void setRequestID(int requestID) { this.requestID = requestID; }
+
+    public void setClientName(String clientName) {this.clientName = clientName;}
+
+    public void setPickUpLocation(String pickUpLocation) {this.pickUpLocation = pickUpLocation;}
+
+    public void setDropOffLocation(String dropOffLocation) {this.dropOffLocation = dropOffLocation;}
+
+    public void setPassengerCount(int passengerCount) {this.passengerCount = passengerCount;}
+
+    public void setSpecialRequirements(String specialRequirements) {this.specialRequirements = specialRequirements;}
+
+    public void setRequestDate(Date requestDate) {this.requestDate = requestDate;}
+
+    public void setPickupTime(Time pickupTime) {this.pickupTime = pickupTime;}
+
+    public void setStatus(String status) {this.status = status;}
 }
